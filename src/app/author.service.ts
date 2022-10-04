a19cec0bde8dc5b7f01f53af59833f9e5e595faf
imlake0701 <39432174+imlake0701@users.noreply.github.com>
2022-10-04 09:52:41 +0800

    Initial commit

---
+++ src/app/author.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthorService {
+  getAuthors() {
+    return ['author1', 'author2', 'author3'];
+  }
+  // constructor() { }
+}
